7b079a86129244bdec45f455291a8c2a73cac4a0
535220166 <149558071+ChristabellaJC@users.noreply.github.com>
2023-11-27 00:40:36 +0700

    Revert "Revert "Sign in sign up""

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_uas_testing/functions/bottomnavbar.dart';
-import 'package:flutter_uas_testing/screens/mainpage.dart';
+
 import 'signup.dart';
-import 'package:get/get.dart';
+import '../functions/text_input.dart';
+import '../functions/password_input.dart';
+import '../../utils/sizes.dart';
 
 class SignInPage extends StatefulWidget {
   @override
@@ -16,55 +17,116 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.all(28.0),
+        padding:
+            EdgeInsets.only(top: TSizes.topPad, bottom: TSizes.botPad, left: TSizes.leftPad, right: TSizes.rightPad),
         child: Column(
           children: [
-            Text(
-              'Sign In',
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 229, 237, 255)),
-            ),
+
+            //JUDUL
+            Text('Sign In',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
             Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: buildTextField('Email', email),
+              padding: const EdgeInsets.only(top: TSizes.defPad),
+
+              //EMAIL
+              child: MakeTextField(label: 'Email', controller: email),
             ),
+
             SizedBox(height: 10),
-            buildPasswordTextField(),
+
+            //PASSWORD
+            PasswordTextField(controller: passw),
+
+            //FORGOT PASS
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    print('ini forgot pass');
+                  },
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      color: Color(0xFF717CE2),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            
             Padding(
               padding: const EdgeInsets.only(bottom: 20.0),
               child: SizedBox(height: 10),
             ),
+
+            //BUTTON SIGN IN
             ElevatedButton(
-              onPressed: () => Get.to(() => const NavBar()),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 229, 237, 255),
-              ),
-              child: Text(
-                'Sign In',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 62, 71, 114),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextButton(
               onPressed: () {
-                print('ini forgot pass');
+                print('ini sign in button');
               },
-              child: Text(
-                'Forgot Password?',
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
-                  color: Color.fromARGB(255, 229, 237, 255),
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF717CE2),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
+                fixedSize: Size(200.0, 35.0),
               ),
+              child: Text('Sign In'),
             ),
+
+            SizedBox(height: 20),
+            
+            //Sign In with Other
+            Row(
+              children:[
+                Flexible(child: Divider(color:Colors.grey, thickness: 0.5, indent: 60, endIndent: 5,)),
+                Text("Or Sign In With", style: TextStyle(fontSize: 12)),
+                Flexible(child: Divider(color:Colors.grey, thickness: 0.5, indent: 5, endIndent: 60,)),
+              ]
+            ),
+
+            SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(40)),
+                  child: IconButton(
+                    onPressed: (){},
+                    icon: const Image(
+                      width: 100,
+                      height: 100,
+                      image: AssetImage('assets/ImgGoogle.png'),
+                    )
+                  )
+                ),
+                SizedBox(width: 15),
+                Container(
+                  decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(40)),
+                  child: IconButton(
+                    onPressed: (){},
+                    icon: const Image(
+                      width: 50,
+                      height: 50,
+                      image: AssetImage('assets/ImgFacebook.png'),
+                    )
+                  )
+                )
+              ],
+            ),
+
+            Spacer(),
+            
+            //SIGN UP
             TextButton(
               onPressed: () {
-                print('ini signin');
+                print('ini signup');
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => SignUpPage()),
@@ -74,55 +136,11 @@ class _SignInPageState extends State<SignInPage> {
                 "Don't have an account? Sign Up",
                 style: TextStyle(
                   decoration: TextDecoration.underline,
-                  color: Color.fromARGB(255, 229, 237, 255),
+                  color: Color(0xFF717CE2),
                 ),
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildTextField(String label, TextEditingController controller) {
-    return TextField(
-      controller: controller,
-      style: TextStyle(color: Color.fromARGB(255, 229, 237, 255)),
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: TextStyle(color: Color.fromARGB(255, 229, 237, 255)),
-        filled: true,
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Color.fromARGB(255, 229, 237, 255)),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Color.fromARGB(255, 229, 237, 255)),
-        ),
-      ),
-    );
-  }
-
-  Widget buildPasswordTextField() {
-    return TextField(
-      controller: passw,
-      obscureText: true,
-      style: TextStyle(color: Color.fromARGB(255, 229, 237, 255)),
-      decoration: InputDecoration(
-        labelText: 'Password',
-        labelStyle: TextStyle(color: Color.fromARGB(255, 229, 237, 255)),
-        filled: true,
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Color.fromARGB(255, 229, 237, 255)),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Color.fromARGB(255, 229, 237, 255)),
-        ),
-        suffixIcon: IconButton(
-          icon: Icon(Icons.visibility),
-          color: Color.fromARGB(255, 229, 237, 255),
-          onPressed: () {
-            print('ini untuk visibility');
-          },
         ),
       ),
     );
---
+++ lib/utils/sizes.dart
@@ -0,0 +1,22 @@
+class TSizes {
+  // Font sizes
+  static const double fontTitle = 24.0;
+
+  // Padding and margin sizes
+  static const double defPad = 24.0;
+  static const double topPad = 40.0;
+  static const double botPad = 50.0;
+  static const double leftPad = 28.0;
+  static const double rightPad = 28.0;
+
+  // Button sizes
+  static const double buttonHeight = 35.0;
+  static const double buttonRadius = 20.0;
+  static const double buttonWidth = 200.0;
+  static const double buttonElevation = 4.0;
+
+  // Default spacing between sections
+  static const double defaultSpace = 24.0;
+  static const double spaceBtwItems = 16.0;
+  static const double spaceBtwSections = 32.0;
+}
