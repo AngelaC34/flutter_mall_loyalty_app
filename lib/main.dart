f76245a3d3e663ea17f517a6e481f185f4365641
AngelaC34 <149558767+AngelaC34@users.noreply.github.com>
2023-11-29 22:53:13 +0700

    firebase web

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_uas_testing/utils/colors.dart';
 import 'screens/firstpage.dart';
 import 'package:get/get.dart';
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 // import 'package:firebase_core/firebase_core.dart';
 // import 'signin.dart';
 
@@ -32,7 +39,17 @@ import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyC9KW_nNL0CYTlEHqYqoGW8xh4dtf8E_zA",
+        authDomain: "uas-flutter-eade1.firebaseapp.com",
+        projectId: "uas-flutter-eade1",
+        storageBucket: "uas-flutter-eade1.appspot.com",
+        messagingSenderId: "52048075633",
+        appId: "1:52048075633:web:5fdd30d443c0e4c4f074d0",
+        measurementId: "G-P8S20Y5BKY"
+      ),
+  );
   runApp(MyApp());
 }
 
