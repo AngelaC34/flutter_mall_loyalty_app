e29d93d690b7f4f7357b3da1aee8671d706813cf
AngelaC34 <149558767+AngelaC34@users.noreply.github.com>
2023-11-29 15:31:41 +0700

    Added Summary

--- lib/screens/menus/vouchers_page.dart
+++ lib/screens/menus/vouchers_page.dart
@@ -14,36 +14,34 @@ class _VoucherPageState extends State<VoucherPage> {
   List items = [
     CardItems(
         imageUrl: 'assets/vouchers/voucher1.jpg',
-        cardName: 'Voucher 1',
-        summary:
-            'Siapakah itu Gregor Samsa? tentu pria terbaik dan terganteng seluruh dunia, emang ada yang lebih cakep daripada Gregor? ga ada tuh, yg bilang ada jelas jelas bohong dan buktinya ada!! Gregor tuh berumur 35 thn dan dari perusahaan Limbus Company, dia sinner ke 12 dan dibuat based on buku dari Franz Kafka berjudul metamorphosis. Chapter storynya tuh pas Canto I yang nurut ku sih sayang bgt, karena Canto I chapter paling pertama alias chapter tutorial, Gregor backstorynya ga terlalu diexplore. Tau ga sih? Gregor tuh dulunya war vet loh, dia partisipasi dalam Smoke War yang terjadi karena dia dulunya kerja untuk G Corp yang sekarang sudah g ada nah G corp ini lawan ama L corp lama dan krn itu dia dimutasi jadi kecoa biar kuat gitu loh, semua ID Gregor rata rata confident smug and badass tapi cuman base Gregor aja yang ga gitu alasannya tuh karena Gregor merasa self conscious akan bug bugnya :( kasihan kan aww my poor poor Gregor #gregorfanforlife.',
-        location: 'Limbus Company',
+        cardName: 'DELICIOUS DEALS IN COLLABORATION WITH TIKTOK',
+        summary: 'Get Rp 50K F&B voucher on TikTok!',
+        location: 'Info Desk, Laguna Atrium, GF',
         cardTime: '09.30 - 11.00',
-        cardDate: '12-12-12'),
+        cardDate: '06 - 12 Nov 2023'),
     CardItems(
         imageUrl: 'assets/vouchers/voucher2.jpg',
-        cardName: 'Voucher 2',
-        summary:
-            'Siapakah itu Gregor Samsa? tentu pria terbaik dan terganteng seluruh dunia, emang ada yang lebih cakep daripada Gregor? ga ada tuh, yg bilang ada jelas jelas bohong dan buktinya ada!! Gregor tuh berumur 35 thn dan dari perusahaan Limbus Company, dia sinner ke 12 dan dibuat based on buku dari Franz Kafka berjudul metamorphosis. Chapter storynya tuh pas Canto I yang nurut ku sih sayang bgt, karena Canto I chapter paling pertama alias chapter tutorial, Gregor backstorynya ga terlalu diexplore. Tau ga sih? Gregor tuh dulunya war vet loh, dia partisipasi dalam Smoke War yang terjadi karena dia dulunya kerja untuk G Corp yang sekarang sudah g ada nah G corp ini lawan ama L corp lama dan krn itu dia dimutasi jadi kecoa biar kuat gitu loh, semua ID Gregor rata rata confident smug and badass tapi cuman base Gregor aja yang ga gitu alasannya tuh karena Gregor merasa self conscious akan bug bugnya :( kasihan kan aww my poor poor Gregor #gregorfanforlife.',
-        location: 'Limbus Company',
+        cardName: 'THIS IS APRIL Get Voucher IDR 50K No Min. Purchase',
+        summary: 'THIS IS APRIL GET VOUCHER Rp 50,000* No min purchase',
+        location: 'L3 - 106',
         cardTime: '09.30 - 11.00',
-        cardDate: '12-12-12'),
+        cardDate: '10 - 28 Nov 2023'),
     CardItems(
         imageUrl: 'assets/vouchers/voucher3.jpg',
-        cardName: 'Voucher 3',
+        cardName: "LEVI'S Get The Levi's Shopping Voucher Up To IDR 500K",
         summary:
-            'Siapakah itu Gregor Samsa? tentu pria terbaik dan terganteng seluruh dunia, emang ada yang lebih cakep daripada Gregor? ga ada tuh, yg bilang ada jelas jelas bohong dan buktinya ada!! Gregor tuh berumur 35 thn dan dari perusahaan Limbus Company, dia sinner ke 12 dan dibuat based on buku dari Franz Kafka berjudul metamorphosis. Chapter storynya tuh pas Canto I yang nurut ku sih sayang bgt, karena Canto I chapter paling pertama alias chapter tutorial, Gregor backstorynya ga terlalu diexplore. Tau ga sih? Gregor tuh dulunya war vet loh, dia partisipasi dalam Smoke War yang terjadi karena dia dulunya kerja untuk G Corp yang sekarang sudah g ada nah G corp ini lawan ama L corp lama dan krn itu dia dimutasi jadi kecoa biar kuat gitu loh, semua ID Gregor rata rata confident smug and badass tapi cuman base Gregor aja yang ga gitu alasannya tuh karena Gregor merasa self conscious akan bug bugnya :( kasihan kan aww my poor poor Gregor #gregorfanforlife.',
-        location: 'Limbus Company',
+            "Join Levi's®️ Red Tab member program and receive Claw Machine vouchers up to Rp500,000. It's time to play, and get the Levi's shopping voucher shop!",
+        location: 'UG - 109',
         cardTime: '09.30 - 11.00',
         cardDate: '12-12-12'),
     CardItems(
         imageUrl: 'assets/vouchers/voucher4.jpg',
-        cardName: 'Voucher 4',
+        cardName: 'SEPHORA HOLIDAY JOYFUL GIFT',
         summary:
-            'Siapakah itu Gregor Samsa? tentu pria terbaik dan terganteng seluruh dunia, emang ada yang lebih cakep daripada Gregor? ga ada tuh, yg bilang ada jelas jelas bohong dan buktinya ada!! Gregor tuh berumur 35 thn dan dari perusahaan Limbus Company, dia sinner ke 12 dan dibuat based on buku dari Franz Kafka berjudul metamorphosis. Chapter storynya tuh pas Canto I yang nurut ku sih sayang bgt, karena Canto I chapter paling pertama alias chapter tutorial, Gregor backstorynya ga terlalu diexplore. Tau ga sih? Gregor tuh dulunya war vet loh, dia partisipasi dalam Smoke War yang terjadi karena dia dulunya kerja untuk G Corp yang sekarang sudah g ada nah G corp ini lawan ama L corp lama dan krn itu dia dimutasi jadi kecoa biar kuat gitu loh, semua ID Gregor rata rata confident smug and badass tapi cuman base Gregor aja yang ga gitu alasannya tuh karena Gregor merasa self conscious akan bug bugnya :( kasihan kan aww my poor poor Gregor #gregorfanforlife.',
-        location: 'Limbus Company',
+            'Unwrapping joy and spreading holiday cheer, one joyful gift at a time. Receive a Sephora Mystery Gift (worth up to IDR 600,000) with minimum spend IDR 2,000,000 on any brand.',
+        location: 'GF - 102 & 103',
         cardTime: '09.30 - 11.00',
-        cardDate: '12-12-12'),
+        cardDate: 'until 23 November 2023'),
   ];
   @override
   Widget build(BuildContext context) {
@@ -77,7 +75,7 @@ class _VoucherPageState extends State<VoucherPage> {
           itemBuilder: (context, index) {
             return SizedBox(
               width: MediaQuery.of(context).size.width * 0.2,
-              height: MediaQuery.of(context).size.height * 0.5,
+              height: MediaQuery.of(context).size.height * 0.45,
               child: CardBox(cardItems: items[index]),
             );
           },
