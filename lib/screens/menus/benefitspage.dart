999cc1caa3be7afe83f65553e618d7eee0e6a1aa
Jessch22 <jessica.535220187@stu.untar.ac.id>
2023-12-01 00:52:35 +0700

    Make string<list>

--- lib/screens/menus/benefitspage.dart
+++ lib/screens/menus/benefitspage.dart
@@ -15,26 +15,33 @@ class _BenefitsPageState extends State<BenefitsPage> {
         cardColor: Color.fromARGB(255, 105, 65, 53),
         title: 'Bronze',
         points: '0-99 points',
-        benefit1: '1',
-        benefit2: '2',
-        benefit3: '3',
-        benefit4: '4'),
+        benefits: [
+          'Earn points on purchases', 
+          'Two hours complimentary car parking weekly',
+          'Early access to sales'
+        ],),
     BenefitItems(
         cardColor: Color.fromARGB(255, 68, 68, 68),
         title: 'Silver',
         points: '100-249 points',
-        benefit1: '1',
-        benefit2: '2',
-        benefit3: '3',
-        benefit4: '4'),
+        benefits: [
+          'Earn points on purchases', 
+          'Two hours complimentary car parking week',
+          'Early access to sales'
+          'Extra rewards points on specially selected days',
+          'Priority event registration'
+        ],),
     BenefitItems(
         cardColor: Color.fromARGB(255, 234, 160, 23),
         title: 'Gold',
         points: '250 - 500 points',
-        benefit1: '1',
-        benefit2: '2',
-        benefit3: '3',
-        benefit4: '4'),
+        benefits: [
+          'Earn points on purchases', 
+          'Two hours complimentary car parking daily',
+          'Early access to sales'
+          'Extra rewards points on specially selected days',
+          'Priority event registration'
+        ],),
   ];
 
   @override
