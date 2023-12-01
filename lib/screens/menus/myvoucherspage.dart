4c1857794298d61f0105beff9918c3de2794534f
AngelaC34 <149558767+AngelaC34@users.noreply.github.com>
2023-12-01 16:07:02 +0700

    add points function and vouchers page

---
+++ lib/screens/menus/myvoucherspage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyVouchers extends StatefulWidget {
+  const MyVouchers({super.key});
+
+  @override
+  State<MyVouchers> createState() => _MyVouchersState();
+}
+
+class _MyVouchersState extends State<MyVouchers> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
