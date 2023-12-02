e4288c58340670601997f2fdfe04e356c61aa007
wontonsouper <149463477+wontonsouper@users.noreply.github.com>
2023-12-02 23:31:45 +0700

    Update aboutus.dart

--- lib/screens/menus/aboutus.dart
+++ lib/screens/menus/aboutus.dart
@@ -12,6 +12,36 @@ class AboutUsItem {
   bool isExpanded;
 }
 
+List<AboutUsItem> getAboutUsItems() {
+  return [
+    AboutUsItem(
+      headerText: 'Tentang Central Park Mall',
+      expandedText:
+          'Central Park Mall adalah destinasi perbelanjaan dan hiburan terkemuka yang menggabungkan gaya hidup modern dengan kenyamanan yang tak tertandingi. Berlokasi strategis di pusat kota, kami menjadi pilihan utama bagi mereka yang mencari pengalaman berbelanja yang eksklusif dan beragam.',
+    ),
+    AboutUsItem(
+      headerText: 'Visi dan Misi Kami',
+      expandedText:
+          'Visi kami adalah menciptakan ruang yang unik, di mana keindahan arsitektur modern bertemu dengan keberagaman penawaran produk dan hiburan yang tak terbatas. Kami berkomitmen untuk memberikan pengalaman belanja yang memuaskan dan menyenangkan bagi setiap pengunjung. Misi kami adalah menjadi pusat gaya hidup terdepan yang menyediakan berbagai layanan dan produk berkualitas tinggi. Kami berusaha untuk memberikan nilai tambah bagi komunitas kami dengan mendukung pertumbuhan ekonomi lokal dan memberdayakan pelanggan serta mitra bisnis kami.',
+    ),
+    AboutUsItem(
+      headerText: "Fasilitas dan Layanan",
+      expandedText:
+          "Central Park Mall menawarkan lebih dari sekadar tempat berbelanja. Dengan ruang parkir yang luas, akses mudah melalui transportasi umum, dan area bermain anak-anak yang didedikasikan, kami merancang pengalaman yang nyaman dan ramah keluarga. Di dalam mall, Anda akan menemukan beragam toko, mulai dari butik fashion, toko elektronik, salon kecantikan, hingga toko buku, semuanya menawarkan pilihan produk dari merek internasional dan lokal. Untuk kenyamanan pengunjung internasional, kami juga menyediakan layanan penukaran mata uang dengan kurs yang bersaing.",
+    ),
+    AboutUsItem(
+      headerText: 'Berita dan Acara',
+      expandedText:
+          'Jangan lewatkan berbagai acara dan promosi menarik yang kami adakan secara berkala. Dari pameran seni hingga pertunjukan live, Central Park Mall selalu berusaha memberikan hiburan yang segar dan menghibur.',
+    ),
+    AboutUsItem(
+      headerText: "Hubungi Kami",
+      expandedText:
+          "Apakah Anda memiliki pertanyaan atau masukan? Tim kami siap membantu. Kunjungi pusat informasi kami di dalam mall atau hubungi kami melalui website resmi kami. Terima kasih telah memilih Central Park Mall sebagai destinasi belanja dan hiburan Anda. Kami berharap dapat memberikan pengalaman yang tak terlupakan setiap kali Anda berkunjung.",
+    ),
+  ];
+}
+
 class AboutUs extends StatefulWidget {
   const AboutUs({Key? key}) : super(key: key);
   @override
